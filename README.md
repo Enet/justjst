bdfa37703b866abef149c1afb45a2d9a5be7dc2a
Stepan Zhevak <enet@protonmail.ch>
2015-10-12 16:52:28 +0300

    Browser support was added

---
+++ README.md
@@ -0,0 +1,23 @@
+# justjst
+The easiest template engine to compile javascript templates. The module returns template-function, which could be executed any moment and returns the final string.
+
+Are there some options? No. All you can use is constructions like `<? echo('9 + 16 = 25') ?>` and `<?= 2 * 2 === 4 ?>`.
+
+## Usage example
+You may use this module both with Node.js or in the browser.
+```javascript
+'use strict';
+let justjst = require('justjst'),
+    template = justjst('Hello, <? echo(name) ?>!\n2 * 2 = <?= x ?>');
+
+console.log(template({
+    x: 4,
+    name: 'world'
+}));
+
+/*
+Hello, world!
+2 * 2 = 4
+*/
+```
+You may find an example of real use [here](https://github.com/Enet/demo-es2015).
--- index.js
+++ index.js
@@ -1,48 +1,60 @@
-'use strict';
+!function () {
+    'use strict';
 
-let char = [0, 1, 2, 3].map(c => String.fromCharCode(c)),
-    re = Object.assign([0, 1, 2, 3].map(c => new RegExp(char[c], 'gm')), {
-        n: /\n/gm,
-        slash: /\\/gm,
-        quote: /'/gm
-    });
+    let char = [0, 1, 2, 3].map(c => String.fromCharCode(c)),
+        re = Object.assign([0, 1, 2, 3].map(c => new RegExp(char[c], 'gm')), {
+            n: /\n/gm,
+            slash: /\\/gm,
+            quote: /'/gm
+        });
 
-module.exports = function (template) {
-    for (let c = 0, cl = char.length; c < cl; c++) {
-        template = template.replace(re[c], '');
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        module.exports = justjst;
+    } else if (typeof modules === 'object') {
+        modules.define('justjst', provide => provide(justjst));
+    } else if (typeof define === 'function') {
+        define((require, exports, module) => module.exports = justjst);
+    } else {
+        window.justjst = justjst;
     }
-    let jsInserts = [],
-        templateCode = (template + '')
-            .replace(re.n, char[0])
-            .replace(re.slash, char[2])
-            .replace(/<\?(.*?)[^\\]\?>/gm, (match, group) => {
-                group = group
-                    .replace(re[0], '\n')
-                    .replace(re[2], '\\')
-                    .replace(re.slash, '\\\\');
-                jsInserts.push(group[0] === '=' ? `echo(${group.slice(1)})` : group);
-                return char[3];
-            })
-            .split(char[3])
-            .map((string, s) => {
-                return `jst+='${string.replace(/\'/gm,char[1])}';\n${jsInserts[s]||''};\n`;
-            })
-            .join('')
-            .replace(re.quote, '\\\'')
-            .replace(re.n, '\\n'),
-        body = `
-            'use strict';
-            data = data || {};
-            let args = Object.keys(data),
-                body = '"use strict";' +
-                    'let echo=(something)=>{jst+=something;return "";};' +
-                    'let re=[0,1,2].map(c=>new RegExp(String.fromCharCode(c),"gm"));' +
-                    'let jst="";' +
-                    '${templateCode}' +
-                    'return jst.replace(re[0],"\\\\n").replace(re[1],"\\\'").replace(re[2],"\\\\\\\\");',
-                fn = new Function(args.join(','), body);
-            return fn.apply(fn, args.map(key => data[key]));
-        `,
-        fn = new Function('data', body);
-    return fn;
-};
+
+    function justjst (template) {
+        for (let c = 0, cl = char.length; c < cl; c++) {
+            template = template.replace(re[c], '');
+        }
+        let jsInserts = [],
+            templateCode = (template + '')
+                .replace(re.n, char[0])
+                .replace(re.slash, char[2])
+                .replace(/<\?(.*?)[^\\]\?>/gm, (match, group) => {
+                    group = group
+                        .replace(re[0], '\n')
+                        .replace(re[2], '\\')
+                        .replace(re.slash, '\\\\');
+                    jsInserts.push(group[0] === '=' ? `echo(${group.slice(1)})` : group);
+                    return char[3];
+                })
+                .split(char[3])
+                .map((string, s) => {
+                    return `jst+='${string.replace(/\'/gm,char[1])}';\n${jsInserts[s]||''};\n`;
+                })
+                .join('')
+                .replace(re.quote, '\\\'')
+                .replace(re.n, '\\n'),
+            body = `
+                'use strict';
+                data = data || {};
+                let args = Object.keys(data),
+                    body = '"use strict";' +
+                        'let echo=(something)=>{jst+=something;return "";};' +
+                        'let re=[0,1,2].map(c=>new RegExp(String.fromCharCode(c),"gm"));' +
+                        'let jst="";' +
+                        '${templateCode}' +
+                        'return jst.replace(re[0],"\\\\n").replace(re[1],"\\\'").replace(re[2],"\\\\\\\\");',
+                    fn = new Function(args.join(','), body);
+                return fn.apply(fn, args.map(key => data[key]));
+            `,
+            fn = new Function('data', body);
+        return fn;
+    };
+}();
